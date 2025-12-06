140753ceb9a7b04d97ab84ffd8075a89daf222fb
PIYUSH KUMAR ANAND <39540002+piyushanand226@users.noreply.github.com>
2025-12-06 06:40:50 +0530

    Update app.component.ts
    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
 
-// Components
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
-import { HomeComponent } from './home/home.component';
-import { AddcarComponent } from './addcar/addcar.component';
-import { ImageuploadComponent } from './imageupload/imageupload.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-
-// Modules
-import { AppRoutingModule } from './app-routing.module';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
-import { MatButtonModule } from '@angular/material/button';
-import { AgmCoreModule } from '@agm/core';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegistrationComponent,
-    HomeComponent,
-    AddcarComponent,
-    ImageuploadComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
-    TableListComponent,
-    TypographyComponent,
-    UpgradeComponent,
-    UserProfileComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BsDatepickerModule.forRoot(),
-    MatButtonModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppModule { }
-export { AppComponent };
+export class AppComponent {
+  title = 'upcoming-car';
 
+  
+}
